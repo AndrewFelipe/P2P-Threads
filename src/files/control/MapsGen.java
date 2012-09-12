6d346fcb4b74212bc7aac8a8ab9992fee97654b0
Leandro.Battisti <Leandro.Battisti@nbbnu00007.interno.senior.com.br>
2012-09-12 20:04:42 -0300

    map gen e poc de socket

--- src/files/control/MapsGen.java
+++ src/files/control/MapsGen.java
@@ -1,16 +1,31 @@
 package files.control;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 import model.MapFile;
 
 public class MapsGen {
-	private static final int QTDSEG = 5;
+	Properties properties = new Properties();
 	
 	public MapFile getMapFileFromFile(File file){
+		try {
+			properties.load(new FileInputStream("p2pthreads.properties"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		MapFile m = new MapFile();
 		m.setFilename(file.getName());
-		m.setQtdSegments(QTDSEG);
+		int qtdSeg = Integer.valueOf(properties.getProperty("qtdSegment"));
+		m.setQtdSegments(qtdSeg);
 		return m;
 	}
 }
